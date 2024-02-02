7c2306ba91c3bc66ecbc4752251625778168a2d2
Peijun Ma <peijun.ma@protonmail.com>
2024-02-02 15:01:36 -0500

    Initial commit

---
+++ src/main/java/StatsTracker/StatsTracker.java
@@ -0,0 +1,23 @@
+package StatsTracker;
+
+import basemod.BaseMod;
+import basemod.interfaces.PostInitializeSubscriber;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@SpireInitializer
+public class StatsTracker implements PostInitializeSubscriber {
+    public static final Logger logger = LogManager.getLogger(StatsTracker.class.getName());
+
+    public StatsTracker() {
+        BaseMod.subscribe(this);
+    }
+
+    public static void initialize() {
+    }
+
+    @Override
+    public void receivePostInitialize() {
+    }
+}
