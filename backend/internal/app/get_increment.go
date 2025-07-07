7850c52f6cb7595b820d18b98bbc1bf486c97fb1
Peijun Ma <peijun.ma@protonmail.com>
2025-07-06 20:07:53 -0400

    Convert get increment to use url parameters

--- backend/internal/app/get_increment.go
+++ backend/internal/app/get_increment.go
@@ -3,15 +3,9 @@ package app
 import (
 	"encoding/json"
 	"net/http"
-	"net/url"
+	"strconv"
 	"time"
 )
-
-type GetIncrementRequest struct {
-	Profile       string `json:"profile"`
-	GameVersion   string `json:"gameVersion"`
-	SchemaVersion int    `json:"schemaVersion"`
-}
 
 type GetIncrementResponse struct {
 	LastRunTime *time.Time  `json:"lastRunTime"`
@@ -23,31 +17,30 @@ func (app *App) handleGetIncrement(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		return
 	}
-	var req GetIncrementRequest
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		app.logger.Warn("Failed to decode get increment request", "error", err)
-		http.Error(w, "Invalid JSON in request body", http.StatusBadRequest)
-		return
-	}
 
-	if req.Profile == "" {
-		http.Error(w, "Profile is required", http.StatusBadRequest)
+	profile := r.FormValue("profile")
+	if profile == "" {
+		http.Error(w, "Missing profile", http.StatusBadRequest)
 		return
 	}
-	if req.GameVersion == "" {
-		http.Error(w, "GameVersion is required", http.StatusBadRequest)
+	gameVersion := r.FormValue("game-version")
+	if gameVersion == "" {
+		http.Error(w, "Missing game-version", http.StatusBadRequest)
 		return
 	}
-	if req.SchemaVersion == 0 {
-		http.Error(w, "SchemaVersion is required", http.StatusBadRequest)
+	schemaVersionStr := r.FormValue("schema-version")
+	if schemaVersionStr == "" {
+		http.Error(w, "Missing schema-version", http.StatusBadRequest)
 		return
 	}
-
-	// URL encode the profile
-	encodedProfile := url.QueryEscape(req.Profile)
+	schemaVersion, err := strconv.Atoi(schemaVersionStr)
+	if err != nil || schemaVersion < 1 {
+		http.Error(w, "Invalid schema version", http.StatusBadRequest)
+		return
+	}
 
 	last, outdated, err := app.db.QueryIncrement(r.Context(),
-		user.Username, encodedProfile, req.GameVersion, req.SchemaVersion)
+		user.Username, profile, gameVersion, schemaVersion)
 	if err != nil {
 		app.logger.Warn("Failed to get increment", "error", err)
 		http.Error(w, "Failed to get increment", http.StatusInternalServerError)
