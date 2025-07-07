d69f7aa9e293646bb6582061fee18b637b4ee35a
Peijun Ma <peijun.ma@protonmail.com>
2025-07-06 21:32:39 -0400

    Implement auto sync

---
+++ src/main/java/StatsTracker/patches/MetricsPatch.java
@@ -0,0 +1,30 @@
+package StatsTracker.patches;
+
+import StatsTracker.StatsTracker;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.megacrit.cardcrawl.metrics.Metrics;
+
+public class MetricsPatch {
+    @SpirePatch2(clz = Metrics.class, method = "gatherAllDataAndSave")
+    public static class Patch {
+        @SpirePostfixPatch
+        public static void Postfix(Metrics __instance) {
+            StatsTracker.logger.info("running gatherAllDataAndSave patch");
+            StatsTracker instance = StatsTracker.instance;
+            if (instance == null) {
+                StatsTracker.logger.error("StatsTracker instance is null");
+                return;
+            }
+            if (instance.config == null) {
+                StatsTracker.logger.error("StatsTracker config is null");
+                return;
+            }
+            if (!instance.config.getAutoSync()) {
+                StatsTracker.logger.info("AutoSync disabled");
+                return;
+            }
+            instance.uploadIncrementalRuns(5);
+        }
+    }
+}
